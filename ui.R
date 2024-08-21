d2a1dec5082d34a6ee070ba78b2343af943ebcac
jshi0959 <jshi0959@uni.sydney.edu.au>
2024-08-21 14:27:17 +1000

    merge bug fix

--- ui.R
+++ ui.R
@@ -1,45 +1,47 @@
 library(shiny)
+library(jpeg)
 
-ui <- navbarPage("CS29",
-                 tabPanel("Home",
-
-                          htmlTemplate("www/home.html")
-
-                 ),
-                 tabPanel("algorithm ",
-                          fluidPage(
-                            sidebarLayout(
-                              sidebarPanel(
-                                userInput_ui("userInput"),
-                                obstaclesUI("obstacles")
-                              ),
-                              mainPanel(
-                                uiOutput("dynamicUI")
-                              )
-                            )
-                          )
-                 ),
-                 tabPanel("Gallery",
-
-                          htmlTemplate("www/Design_Gallery.html")
-
-                 ),
-                 tabPanel("Design",
-
-                          htmlTemplate("www/customer_design.html")
-
-                 ),
-                 tabPanel("About Us",
-                          fluidPage(
-                            titlePanel("About Us"),
-                            p("This is the About Us page.")
-                          )
-                 ),
-                 tabPanel("Help",
-                          fluidPage(
-                            titlePanel("Help"),
-                            p("This is the Help page.")
-                          )
-                 )
+ui <- navbarPage(
+  "Tile app",
+  tabPanel(
+    "Home",
+    htmlTemplate("www/home.html")
+  ),
+  tabPanel(
+    "Gallery",
+    htmlTemplate("www/Design_Gallery.html")
+  ),
+  tabPanel("algorithm ",
+           fluidPage(
+             sidebarLayout(
+               sidebarPanel(
+                 userInput_ui("userInput"),
+                 obstaclesUI("obstacles")
+               ),
+               mainPanel(
+                 uiOutput("dynamicUI")
+               )
+             )
+           )
+  ),
+  tabPanel(
+    "Design",
+    htmlTemplate("www/customer_design.html")
+  ),
+  tabPanel(
+    "Gallery",
+    htmlTemplate("www/Design_Gallery.html")
+  ),
+  tabPanel(
+    "Design",
+    htmlTemplate("www/customer_design.html")
+  ),
+  tabPanel(
+    "About Us",
+    htmlTemplate("www/about_us.html")
+  ),
+  tabPanel(
+    "Help",
+    htmlTemplate("www/help.html")
+  ),
 )
-
