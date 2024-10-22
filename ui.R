2fb1740cf64c5d373883d2cb9c242178bd3e9aaa
jliu6086 <jliu6086@uni.sydney.edu.au>
2024-10-22 13:37:28 +1100

    hide the old design section and remove the redundant code

--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(jpeg)
+library(colourpicker)
 source("R/designUI.R")
 
 ui <- navbarPage(
@@ -7,16 +8,32 @@ ui <- navbarPage(
   "Tile app",
   tabPanel("Home", htmlTemplate("www/home.html")),
   tabPanel("Gallery", htmlTemplate("www/design_gallery.html")),
-  tabPanel("Design", fluidPage(
-    sidebarLayout(
-      sidebarPanel(userInput_ui("userInput"), obstaclesUI("obstacles")),
-      mainPanel(uiOutput("dynamicUI"))
-    )
-  )),
+  tabPanel(
+    "Design",
+    uiOutput("new_ui_container")
+  ),
   tabPanel("About Us", htmlTemplate("www/about_us.html")),
   tabPanel("Help", htmlTemplate("www/help.html")),
-  tabPanel(
-    "New UI",
-    uiOutput("new_ui_container")
-  )
+  
+  
+  
+  
+  
+  
+  tabPanel("", fluidPage(
+    tags$head(
+      tags$style(HTML("
+      #userInputDiv, #obstaclesDiv {
+        display: none !important;  /* 完全隐藏，不占用空间 */
+      }
+    "))
+    ),
+    sidebarLayout(
+      sidebarPanel(
+        div(id = "userInputDiv", userInput_ui("userInput")),
+        div(id = "obstaclesDiv", obstaclesUI("obstacles"))
+      ),
+      mainPanel(uiOutput("dynamicUI"))
+    )
+  ))
 )
